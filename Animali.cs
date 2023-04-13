1dedf646f1bcad0ee327eb6c07ecf3b112290130
Francesco Limpias <francescolimpias@gmail.com>
2023-04-13 14:21:11 +0200

    Added base Project directory structure

---
+++ Animali.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal class Cane
+    {
+    }
+
+    internal class Passerotto
+    {
+
+    }
+
+    internal class Acquila
+    {
+
+    }
+
+    internal class Delfino
+    {
+
+    }
+}
