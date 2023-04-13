97c2d8aebce2069e110ee0f3a91b8bc4884609c8
Francesco Limpias <francescolimpias@gmail.com>
2023-04-13 15:15:16 +0200

    Added testing

--- Program.cs
+++ Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+
+        static List<Animale> animali = new List<Animale>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            animali.Add(new Cane("lupo"));
+            animali.Add(new Passerotto("domestico"));
+            animali.Add(new Aquila("reale"));
+            animali.Add(new Delfino("striato"));
+
+            foreach (Animale animale in animali)
+            {
+                animale.Print();
+                Console.WriteLine("-------------");
+            }
         }
     }
 }
