26390f1dc8550399b0e823cdcf9c2185ba92eb85
Francesco Limpias <francescolimpias@gmail.com>
2023-04-13 14:38:33 +0200

    Added "Animale" abstract class

--- Animale.cs
+++ Animale.cs
@@ -9,5 +9,22 @@ namespace csharp_abstract_animals
     internal abstract class Animale
     {
 
+        /* "Dormi" Function
+         * it is common to all animals
+         */
+        public void Dormi()
+        {
+            Console.WriteLine("Zzz");
+        }
+
+        /* "Verso" Function
+         * it's specific to each animal
+         */
+        public abstract void Verso();
+
+        /* "Mangia" Function
+         */
+        public abstract void Mangia();
+
     }
 }
